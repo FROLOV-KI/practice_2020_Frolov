9c5f93cb4751d6c5ff2fc9210398e658f1b72cb1
Frolov <Frolov.Kirill.17@yandex.ru>
2020-02-08 18:00:05 +0300

    1.Добавлена генерация новых шариков 1 раз в секунду, но не более 10. 2.Добавлены очки и условие победы по ним.

--- Lab3/ball_game_v1.py
+++ Lab3/ball_game_v1.py
@@ -2,21 +2,20 @@ from tkinter import*
 from random import randrange as rnd, choice
 import time
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1366
+HEIGHT = 700
 
 
 class Ball:
-    def __init__(self, name):
-        self.name = "Ball " + str(name)
+    def __init__(self):
         self.colors = ['red', 'orange', 'yellow', 'green', 'blue']
-        self.r = rnd(30, 50)
+        self.r = rnd(10, 50)
         self.x = rnd(self.r, WIDTH - self.r)
         self.y = rnd(self.r, HEIGHT - self.r)
         self.ball_id = canvas.create_oval((self.x - self.r, self.y - self.r), (self.x + self.r, self.y + self.r),
                                           fill=choice(self.colors), width=0)
-        self.dx = rnd(1,6)
-        self.dy = rnd(1,6)
+        self.dx = rnd(-6, 6)
+        self.dy = rnd(-6, 6)
 
     def move(self):
         self.x += self.dx
@@ -39,21 +38,32 @@ class Ball:
         canvas.delete(self.ball_id)
 
     def info(self):
-        return self.name, self.x, self.y, self.r
+        return self.x, self.y, self.r
 
 
 def canvas_click(event):
     """Обработка клика левой кнопки мыши, регистрация поавдвния по шарику"""
+    points = 0
     for ball in balls:
         target = ball.info()
-        distance = ((target[1] - event.x) ** 2 + (target[2] - event.y) ** 2) ** 0.5
-        if distance <= target[3]:
-            print('Got {}!'.format(target[0]))
+        distance = ((target[0] - event.x) ** 2 + (target[1] - event.y) ** 2) ** 0.5
+        if distance <= target[2]:
+            print('Got {} points!'.format(target[2]))
             ball.is_shoot()
             balls.remove(ball)
+            points += 50 - target[2]
+            if not balls or points >= 500:
+                print('Победа')
+                exit()
             break
     else:
         print('miss :(')
+
+
+def make_new_balls():
+    if len(balls) < 10:
+        balls.append(Ball())
+    root.after(1000, make_new_balls)
 
 
 def tick():
@@ -64,14 +74,15 @@ def tick():
 
 
 def main():
-    global root, canvas, balls
-
+    global root, canvas, balls, num
     root = Tk()
     root.geometry(str(WIDTH) + "x" + str(HEIGHT))
     canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg='white')
     canvas.pack(anchor="nw", fill=BOTH)
     canvas.bind('<Button-1>', canvas_click)
-    balls = [Ball(i) for i in range(5)]
+    balls = []
+    num = 0
+    make_new_balls()
     tick()
     mainloop()
 
